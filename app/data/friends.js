eb5338a9a982e55efe7f4a26412c1ff66f73ea64
daniel <dannygrams@yahoo.com>
2018-11-17 17:06:57 -0800

    added everything

---
+++ app/data/friends.js
@@ -0,0 +1,19 @@
+var friends = [
+  {
+      name: "jack",
+      scores: [
+          "4",
+          "2",
+          "2",
+          "5",
+          "1",
+          "3",
+          "4",
+          "3",
+          "1",
+          "2",
+      ]
+  }
+];
+
+module.exports = friends;
